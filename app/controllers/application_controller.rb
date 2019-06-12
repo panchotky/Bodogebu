440331a1391c798ac1c479969d226a9e5825a9fb
panchotky <panchotkycareer@gmail.com>
2019-06-12 05:20:53 +0000

    [Add]headerにモーダルで検索バー表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+   before_action :set_search
+
+   def search
+	    @search = Game.search(search_params)
+	    @search_games = @q.result(distinct: true)
+   end
+
+
+
+   protected
+
+	  def  set_search
+	       @search = Game.ransack(params[:q])
+	       @search_games = @search.result(distinct: true).order(id: :desc)
+      end
 end
